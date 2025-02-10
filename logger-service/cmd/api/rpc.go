01b77f6c0b91ca6ae291e188b9a8bd6c21bac6f0
lhuynhDev <huynhlong020700@gmail.com>
2025-02-10 16:57:08 +0700

    add RPC

--- logger-service/cmd/api/rpc.go
+++ logger-service/cmd/api/rpc.go
@@ -7,26 +7,30 @@ import (
 	"time"
 )
 
-type RPCServer struct {
-}
+// RPCServer is the type for our RPC Server. Methods that take this as a receiver are available
+// over RPC, as long as they are exported.
+type RPCServer struct{}
 
+// RPCPayload is the type for data we receive from RPC
 type RPCPayload struct {
 	Name string
 	Data string
 }
 
-func (r *RPCServer) LogInfo(payload RPCPayload, response *string) error {
-	collection := client.Database("logger").Collection("logs")
+// LogInfo writes our payload to mongo
+func (r *RPCServer) LogInfo(payload RPCPayload, resp *string) error {
+	collection := client.Database("logs").Collection("logs")
 	_, err := collection.InsertOne(context.TODO(), data.LogEntry{
 		Name:      payload.Name,
 		Data:      payload.Data,
 		CreatedAt: time.Now(),
 	})
 	if err != nil {
-		log.Println("Error inserting log entry: ", err)
+		log.Println("error writing to mongo", err)
 		return err
 	}
 
-	*response = " Processed payload by RPC: " + payload.Name
+	// resp is the message sent back to the RPC caller
+	*resp = "Processed payload via RPC:" + payload.Name
 	return nil
 }
